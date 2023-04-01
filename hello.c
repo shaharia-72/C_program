dda452a182a3eb0f434bd8211273f0a02b52e3ee
Shaharia <shahariahossain72@gmail.com>
2023-04-01 14:22:22 +0600

    second program

--- hello.c
+++ hello.c
@@ -4,3 +4,17 @@ int main()
     printf("hello world");
     return 0;
 }
+
+// …or create a new repository on the command line
+// echo "# C_program" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/shaharia-72/C_program.git
+// git push -u origin main
+
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/shaharia-72/C_program.git
+// git branch -M main
+// git push -u origin main
